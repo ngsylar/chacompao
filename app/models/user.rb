92e464d0203e630e834bc571c719266ad3307e5a
ngsylar <ngsylar@gmail.com>
2020-12-29 22:21:16 -0300

    user settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :role, presence: true
+  has_many :versions, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
