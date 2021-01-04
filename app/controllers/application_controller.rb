97d9d0faca1fa329bcc61b67eb347b3438eb2f6d
ngsylar <ngsylar@gmail.com>
2021-01-04 13:42:38 -0300

    update username bugfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
   
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name role])
+        devise_parameter_sanitizer.permit(:account_update, keys: %i[full_name])
     end
 end
